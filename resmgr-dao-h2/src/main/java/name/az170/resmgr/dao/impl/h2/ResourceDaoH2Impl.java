0ee11ac775f4275e83d575b562b09210c5ba27ab
chenzhen <zhen.chen@cn.iisigroup.com>
2016-07-25 16:33:05 +0800

    update

--- resmgr-dao-h2/src/main/java/name/az170/resmgr/dao/impl/h2/ResourceDaoH2Impl.java
+++ resmgr-dao-h2/src/main/java/name/az170/resmgr/dao/impl/h2/ResourceDaoH2Impl.java
@@ -21,11 +21,14 @@ public class ResourceDaoH2Impl implements ResourceDao {
     
     @Autowired
     private JdbcTemplate jt;
+    
+    private String createSql;
+    private String deleteSql;
+    private String updateSql;
 
     @Override
     public int create(final Resource resource) {
-        String sql = MessageFormat.format("INSERT INTO {0} ({1}, {2}, {3}, {4}, {5}) WHERE VALUES (?, ?, ?, ?, ?)",
-                RESOURCES, BASE_ID, RESOURCES_NAME, BASE_CREATE_TIME, BASE_UPDATE_TIME, BASE_COMMENT);
+        String sql = getCreateSql();
         int result = jt.update(sql, new PreparedStatementSetter() {
             @Override
             public void setValues(PreparedStatement ps) throws SQLException {
@@ -41,21 +44,56 @@ public class ResourceDaoH2Impl implements ResourceDao {
     }
 
     @Override
-    public int delete(String id) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int delete(final String id) {
+        String sql = getDeleteSql();
+        int result = jt.update(sql, id);
+        return result;
     }
 
     @Override
-    public int update(Resource resource) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int update(final Resource resource) {
+        String sql = getUpdateSql();
+        int result = jt.update(sql, new PreparedStatementSetter() {
+            @Override
+            public void setValues(PreparedStatement ps) throws SQLException {
+                int i = 0;
+                ps.setString(i++, resource.getName());
+                ps.setDate(i++, new Date(resource.getCreateTime().getTime()));
+                ps.setDate(i++, new Date(resource.getUpdateTime().getTime()));
+                ps.setString(i++, resource.getComment());
+                ps.setString(i++, resource.getId());
+            }
+        });
+        return result;
     }
 
     @Override
-    public Resource read(String id) {
+    public Resource read(final String id) {
         // TODO Auto-generated method stub
         return null;
+    }
+    
+    private String getCreateSql() {
+        if (createSql == null) {
+            createSql = MessageFormat.format("INSERT INTO {0} ({1}, {2}, {3}, {4}, {5}) WHERE VALUES (?, ?, ?, ?, ?)",
+                    RESOURCES, BASE_ID, RESOURCES_NAME, BASE_CREATE_TIME, BASE_UPDATE_TIME, BASE_COMMENT);
+        }
+        return createSql;
+    }
+    
+    private String getDeleteSql() {
+        if (deleteSql == null) {
+            deleteSql = MessageFormat.format("DELETE FROM {0} WHERE ID = ?", RESOURCES);
+        }
+        return deleteSql;
+    }
+    
+    private String getUpdateSql() {
+        if (updateSql == null) {
+            updateSql = MessageFormat.format("UPDATE {0} SET {1} = ?, {2} = ?, {3} = ?, {4} = ? WHERE ID = ?",
+                    RESOURCES, RESOURCES_NAME, BASE_CREATE_TIME, BASE_UPDATE_TIME, BASE_COMMENT);
+        }
+        return updateSql;
     }
 
 }
