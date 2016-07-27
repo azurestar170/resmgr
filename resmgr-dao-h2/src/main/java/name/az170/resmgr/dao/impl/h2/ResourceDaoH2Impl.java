51b4980823437d0d81ba1f384939fde8ed52b4f8
chenzhen <zhen.chen@cn.iisigroup.com>
2016-07-27 14:09:07 +0800

    update

--- resmgr-dao-h2/src/main/java/name/az170/resmgr/dao/impl/h2/ResourceDaoH2Impl.java
+++ resmgr-dao-h2/src/main/java/name/az170/resmgr/dao/impl/h2/ResourceDaoH2Impl.java
@@ -2,12 +2,15 @@ package name.az170.resmgr.dao.impl.h2;
 
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.text.MessageFormat;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import name.az170.resmgr.core.domain.Resource;
@@ -25,6 +28,9 @@ public class ResourceDaoH2Impl implements ResourceDao {
     private String createSql;
     private String deleteSql;
     private String updateSql;
+    private String selectSql;
+    private String queryByIdSql;
+    private ResourceRowMapper resourceRowMapper;
 
     @Override
     public int create(final Resource resource) {
@@ -69,8 +75,13 @@ public class ResourceDaoH2Impl implements ResourceDao {
 
     @Override
     public Resource read(final String id) {
-        // TODO Auto-generated method stub
-        return null;
+        String sql = getQueryByIdSql();
+        List<Resource> list = jt.query(sql, new Object[] {id}, getResourceRowMapper());
+        Resource result = null;
+        if (list.size() > 0) {
+            result = list.get(0);
+        }
+        return result;
     }
     
     private String getCreateSql() {
@@ -94,6 +105,43 @@ public class ResourceDaoH2Impl implements ResourceDao {
                     RESOURCES, RESOURCES_NAME, BASE_CREATE_TIME, BASE_UPDATE_TIME, BASE_COMMENT);
         }
         return updateSql;
+    }
+    
+    private String getSelectSql() {
+        if (selectSql == null) {
+            selectSql = MessageFormat.format("SELECT {0}, {1}, {2}, {3}, {4} FROM {5} WHERE 1 = 1",
+                    BASE_ID, RESOURCES_NAME, BASE_CREATE_TIME, BASE_UPDATE_TIME, BASE_COMMENT, RESOURCES);
+        }
+        return selectSql;
+    }
+    
+    private String getQueryByIdSql() {
+        if (queryByIdSql == null) {
+            StringBuilder sql = new StringBuilder(getSelectSql());
+            sql.append(" AND id = ?");
+            queryByIdSql = sql.toString();
+        }
+        return queryByIdSql;
+    }
+    
+    private ResourceRowMapper getResourceRowMapper() {
+        if (resourceRowMapper == null) {
+            resourceRowMapper = new ResourceRowMapper();
+        }
+        return resourceRowMapper;
+    }
+    
+    public static class ResourceRowMapper implements RowMapper<Resource> {
+        @Override
+        public Resource mapRow(ResultSet rs, int rowNum) throws SQLException {
+            Resource resource = new Resource();
+            resource.setId(rs.getString(BASE_ID));
+            resource.setName(rs.getString(RESOURCES_NAME));
+            resource.setCreateTime(rs.getDate(BASE_CREATE_TIME));
+            resource.setUpdateTime(rs.getDate(BASE_UPDATE_TIME));
+            resource.setComment(rs.getString(BASE_COMMENT));
+            return resource;
+        }
     }
 
 }
